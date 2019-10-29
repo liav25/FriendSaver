80cd86bf252fc5a5cb42a1627deaeda42ebf7452
Liav <liav25@gmail.com>
2019-10-29 17:14:10 +0200

    Added the Add friend dialog XML and part of code.

---
+++ sadna_ex1/app/src/main/java/liav/com/sadna_ex1/AddFriendDialog.java
@@ -0,0 +1,38 @@
+package liav.com.sadna_ex1;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class AddFriendDialog extends AppCompatDialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view  = inflater.inflate(R.layout.activity_add_friend_dialog, null);
+
+        builder.setView(view).setTitle("Be My Friend !")
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        return builder.create();
+
+
+
+    }
+}
--- sadna_ex1/app/src/main/java/liav/com/sadna_ex1/MainActivity.java
+++ sadna_ex1/app/src/main/java/liav/com/sadna_ex1/MainActivity.java
@@ -2,12 +2,32 @@ package liav.com.sadna_ex1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Initialize TextViews of the Activity
+    private TextView titleWelcome;
+    private TextView introText;
+    //Initialize RecyclerView
+    private RecyclerView friends;
+    //Initialize add friend button
+    private Button addFriend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //connect the XML's views to relative objects
+        titleWelcome = (TextView)findViewById(R.id.title_welcome);
+        introText = (TextView)findViewById(R.id.intro_text);
+        friends = (RecyclerView)findViewById(R.id.friends_rec_view);
+        addFriend = (Button)findViewById(R.id.be_friend_button);
+
     }
 }
